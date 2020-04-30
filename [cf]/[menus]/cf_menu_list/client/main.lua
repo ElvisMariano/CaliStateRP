95f83cc66768a441e8b99e476d161adab7eeaa0f
Paul <56743260+ColsSupreme@users.noreply.github.com>
2020-04-30 17:02:20 +0100

    Add files via upload

---
+++ [cf]/[menus]/cf_menu_list/client/main.lua
@@ -0,0 +1,75 @@
+CF = nil
+
+Citizen.CreateThread(function()
+	while CF == nil do 
+		Citizen.Wait(0)
+		CF = exports['cf'].GetFramework ~= nil and exports['cf'].GetFramework() or nil
+	end
+
+	local MenuType    = 'list'
+	local OpenedMenus = {}
+
+	local openMenu = function(namespace, name, data)
+
+		OpenedMenus[namespace .. '_' .. name] = true
+
+		SendNUIMessage({
+			action    = 'openMenu',
+			namespace = namespace,
+			name      = name,
+			data      = data
+		})
+
+		CF.SetTimeout(200, function()
+			SetNuiFocus(true, true)
+		end)
+
+	end
+
+	local closeMenu = function(namespace, name)
+
+		OpenedMenus[namespace .. '_' .. name] = nil
+		local OpenedMenuCount = 0
+
+		SendNUIMessage({
+			action    = 'closeMenu',
+			namespace = namespace,
+			name      = name,
+			data      = data
+		})
+
+		for k,v in pairs(OpenedMenus) do
+			if v == true then
+				OpenedMenuCount = OpenedMenuCount + 1
+			end
+		end
+
+		if OpenedMenuCount == 0 then
+			SetNuiFocus(false)
+		end
+
+	end
+
+	CF.UI.Menu.RegisterType(MenuType, openMenu, closeMenu)
+
+	RegisterNUICallback('menu_submit', function(data, cb)
+		local menu = CF.UI.Menu.GetOpened(MenuType, data._namespace, data._name)
+
+		if menu.submit ~= nil then
+			menu.submit(data, menu)
+		end
+
+		cb('OK')
+	end)
+
+	RegisterNUICallback('menu_cancel', function(data, cb)
+		local menu = CF.UI.Menu.GetOpened(MenuType, data._namespace, data._name)
+
+		if menu.cancel ~= nil then
+			menu.cancel(data, menu)
+		end
+
+		cb('OK')
+	end)
+
+end)
